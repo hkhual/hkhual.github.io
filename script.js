c51a8bd32704420bfad7ca7e2b1ce45abcc23dad
Hau Khual <haukhual08@gmail.com>
2019-06-26 23:09:53 -0700

    added script.js file

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function myFunction(a, b){
+    return a + b;
+}
+
+console.log(myFunction(35,34));
+
+
+function changeFont(){
+
+    document.getElementById("about").innerHTML = "Paragraph changed.";
+
+}
