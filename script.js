b0384046560632b41d6b2aca15d27f038f5d7ac8
Hau Khual <haukhual08@gmail.com>
2019-07-19 23:31:29 -0700

    updated

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ console.log(myFunction(35,34));
 
 
 function changeFont(){
-
     document.getElementById("about").innerHTML = "Paragraph changed.";
+}
 
+var close = document.querySelector('#close');
+close.onclick = function(){
+    this.parentNode.style.display = 'none';
+
+    
 }
